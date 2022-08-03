0461f087321dc3ebba60f529a9ecd325b40372eb
Felipe Oliveira <felipeperoliveira@outlook.com>
2022-08-02 21:25:45 -0300

    Saving work, but with some errors :/

--- src/atoms.rs
+++ src/atoms.rs
@@ -1,8 +1,49 @@
 #[derive(Debug)]
+pub enum ChemicalSet {
+    AlkaliMetal,
+    AlkaliEarthMetal,
+    Chalcogen,
+    Halogen,
+    NobleGas,
+}
+
+impl ChemicalSet {
+    /// Calculate and return the chemical sets of an element depending on its electrons
+    pub fn from_valence_electrons(electrons_in_valence_orbital: u16, az_quantum_number_in_valence_orbital: u16) -> Vec<ChemicalSet> {
+        // calculate the chemical set based on the value from the electrons on the valence
+        let chemical_set_from_valence_electrons =  match (az_quantum_number_in_valence_orbital, electrons_in_valence_orbital){
+            // l=0, h.l=s, e=1
+            (0, 1) => Some(ChemicalSet::AlkaliMetal),
+            // l=0, h.l=s, e=2
+            (0, 2) => Some(ChemicalSet::AlkaliEarthMetal),
+            // l=1, h.l=p, e=4
+            (1, 4) => Some(ChemicalSet::Chalcogen),
+            // l=1, h.l=p, e=5
+            (1, 5) => Some(ChemicalSet::Halogen),
+            // l=1, h.l=p, e=6
+            (1, 6) => Some(ChemicalSet::NobleGas),
+            _ => None,
+        };
+
+        // the vector that will be returned
+        let mut chemical_sets = Vec::<ChemicalSet>::new();
+
+        // store the chemical set if returned some
+        if let Some(chemical_set) = chemical_set_from_valence_electrons {
+            chemical_sets.push(chemical_set);
+        }
+        
+        return chemical_sets
+    }
+}
+
+#[derive(Debug)]
 pub struct ElectronShell {
     electrons : u16,
     electrons_per_shell : Vec<u16>,
-    azimuthal_quantum_numbers_per_shell : Vec<u8>,
+    electrons_per_subshell : Vec<u16>,
+    azimuthal_quantum_numbers_per_subshell : Vec<u8>,
+    //chemical_sets : Vec<ChemicalSet>,
 }
 
 impl ElectronShell {
@@ -11,39 +52,72 @@ impl ElectronShell {
             panic!("The maximum amount of electrons that can be calculated is 60000. {} given", num_of_electrons)
         }
 
-        let mut electrons_per_shell = Vec::<u16>::new();
+        // store total electrons
         let mut electrons : u16 = 0;
-        let mut azimuthal_quantum_numbers_per_shell : Vec<u8> = Vec::new();
-         
+
+        // store shell data
+        let mut electrons_per_shell = Vec::<u16>::new();
+
+        // store subshell data
+        let mut electrons_per_subshell = Vec::<u16>::new();
+        let mut azimuthal_quantum_numbers_per_subshell : Vec<u8> = Vec::new();
         
+        // store the maximum azimuthal quantum number
         let mut max_azimuthal_quantum_number : u16 = 0;
 
-        // go to 1s, 2s, 3s, 4s ...
-         for n in 0..num_of_electrons {
+        let mut max_orbits_per_shell : u16 = 1;
+        let mut current_orbits_per_shell: u16 = 0;
+        let mut electrons_in_current_shell :  u16 = 0;
 
-            if n != 0 && n % 2 == 0 {
+        // go to 1s, 2s, 3s, 4s ...
+         for n_shell_loop in 0..num_of_electrons {
+
+            // for each 2 'n' orbit increase the azimuthal quantum number + 1
+            if n_shell_loop != 0 && n_shell_loop % 2 == 0 {
                 max_azimuthal_quantum_number += 1;
             }
 
-            // iterate from the maximum current value of the azimuthal quantum number to the minimum (0)
+            // calculate subshell values
             for az in (0..=max_azimuthal_quantum_number).rev() {
                 // calculate the maximum amount of electrons in the current azimuthal quantum number
                 // me = 4 * (l + 1) - 2;
-                let mut max_electrons = 4 * (az + 1) - 2;
+                let mut max_electrons_in_subshell = 4 * (az + 1) - 2;
 
                 // store the current azimuthal quantum number
-                azimuthal_quantum_numbers_per_shell.push(az as u8);
+                azimuthal_quantum_numbers_per_subshell.push(az as u8);
 
                 // increase the total amount of counted electrons
-                electrons += max_electrons;
+                electrons += max_electrons_in_subshell;
 
                 // if the electrons count exceeds the limit, decrease it to reach the max electrons
                 if electrons > num_of_electrons {
-                    max_electrons -= electrons - num_of_electrons;
+                    max_electrons_in_subshell -= electrons - num_of_electrons;
                 }
 
                 // include the electrons in the shell counter
-                electrons_per_shell.push(max_electrons);
+                electrons_per_subshell.push(max_electrons_in_subshell);
+
+                // increase the number of electrons per shell and add an orbit in the counter
+                electrons_in_current_shell += max_electrons_in_subshell;
+                current_orbits_per_shell += 1;
+
+                println!("{}-", az);
+                
+                // check if the current orbit reached the max orbits per shell limit or 
+                // reached the limit of electrons in the electrons shell
+                if current_orbits_per_shell == max_orbits_per_shell || electrons >= num_of_electrons{
+
+                    println!("{} <>", az);
+                    // add the total number of electrons in thell
+                    electrons_per_shell.push(electrons_in_current_shell);
+
+                    // reset the sum and the counter
+                    electrons_in_current_shell = 0;
+                    current_orbits_per_shell = 0;
+
+                    // increase the maximum number of orbits per shell
+                    max_orbits_per_shell += 1;
+                }
 
                 // break if reached limit
                 if electrons >= num_of_electrons {
@@ -57,13 +131,27 @@ impl ElectronShell {
             }
          }
 
-        ElectronShell { electrons: num_of_electrons, electrons_per_shell, azimuthal_quantum_numbers_per_shell }
-        
+         // adjust the electron_per_shell octect rule
+         if *electrons_per_shell.last().unwrap() > 8 {
+            let last_electon_shell_value = *electrons_per_shell.last().unwrap();
+            electrons_per_shell.pop();
+            electrons_per_shell.push(8);
+            electrons_per_shell.push(last_electon_shell_value - 8);
+         }
+         
+
+        ElectronShell { 
+            electrons: num_of_electrons, 
+            electrons_per_shell,
+            electrons_per_subshell, 
+            azimuthal_quantum_numbers_per_subshell, 
+            //chemical_sets : ChemicalSet::from_valence_electrons(valence_electrons, valence_az) 
+        }
     }
 
     /// Return the azimuthal quantum number on this the electron shell in order from the closest to the nucleus to the farthest
-    pub fn azimuthal_quantum_numbers_per_shell(&self) -> &Vec<u8> {
-        &self.azimuthal_quantum_numbers_per_shell
+    pub fn azimuthal_quantum_numbers_per_subshell(&self) -> &Vec<u8> {
+        &self.azimuthal_quantum_numbers_per_subshell
     }
 
     /// Return the total amount of electrons from this electron shell
@@ -72,22 +160,27 @@ impl ElectronShell {
     }
 
     /// Return the amount of electrons per shell in order from the closest to the nucleus to the farthest
-    pub fn electrons_per_shell(&self) -> &Vec<u16> {
-        &self.electrons_per_shell
+    pub fn electrons_per_subshell(&self) -> &Vec<u16> {
+        &self.electrons_per_subshell
     }
 
     /// Return an string representation from the shells
-    pub fn shell_to_string(&self) -> String {
-        self.electrons_per_shell
-            .iter().zip(self.azimuthal_quantum_numbers_per_shell.iter())
+    pub fn subshell_to_string(&self) -> String {
+        self.electrons_per_subshell
+            .iter().zip(self.azimuthal_quantum_numbers_per_subshell.iter())
             .map(|(e_value, e_az)| {
                 format!("{}{} ", e_value, subshell_label_from_az_value(*e_az as usize))
             })
             .collect()
-    }   
+    }
+
+    // pub fn chemical_sets(&self) -> &Vec<ChemicalSet> {
+    //     &self.chemical_sets
+    // }
 
 }
 
+#[derive(Debug)]
 pub struct Atom {
     protons : u16,
     neutrons : u16,
@@ -110,16 +203,6 @@ fn subshell_label_from_az_value(az: usize) -> String {
 
 impl Atom {
 
-    pub fn from_pe_n(protons_and_electrons: u16, neutrons : u16) -> Atom {
-        Atom {
-            protons : protons_and_electrons,
-            neutrons,
-            weight : (protons_and_electrons + neutrons) as f64,
-            electrons : protons_and_electrons,
-            electron_shell : ElectronShell::from_electrons(protons_and_electrons)
-        }
-    }
-
     pub fn from_p_e_n(protons : u16, electrons: u16, neutrons: u16) -> Atom {
         Atom {
             protons,
@@ -128,6 +211,14 @@ impl Atom {
             electrons,
             electron_shell : ElectronShell::from_electrons(electrons)
         }
+    }
+
+    pub fn from_pe_n(protons_and_electrons: u16, neutrons : u16) -> Atom {
+        Atom::from_p_e_n(protons_and_electrons, protons_and_electrons, neutrons)
+    }
+
+    pub fn from_pen(protons_electrons_and_neutrons : u16) -> Atom {
+        Atom::from_pe_n(protons_electrons_and_neutrons, protons_electrons_and_neutrons)
     }
 
     /// return electron shell data from this atom
@@ -156,11 +247,12 @@ impl Atom {
     }
 
     /// Return the eletric charge of this atom (p - e)
-    pub fn electric_charge(&self) -> i16 {
+    pub fn ion_electric_charge(&self) -> i16 {
         (self.protons as i32 - self.electrons as i32) as i16
     }
 
+    /// Return an flag indicating if this atom is a isotope of another given atom reference
     pub fn is_isotope_of(&self, other : &Atom) -> bool {
-        (self.protons == other.protons)
+        self.protons == other.protons
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 mod atoms;
 
 fn main() {
-   let h1 = atoms::Atom::from_p_e_n(1, 2, 0);
-   let h2 = atoms::Atom::from_pe_n(1, 1);
 
-   println!("H-1 a.m: {}", h1.atomic_mass());
-   println!("H-1 electric charge: {}", h1.electric_charge());
-   println!("H-2 a.m: {}", h2.atomic_mass());
+   let ne = atoms::Atom::from_pen(108);
+   println!("{:?}", ne.electron_shell());
    
-   println!("H-1 is isotope of H-2: {}", h1.is_isotope_of(&h2));
 }
